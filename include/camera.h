d4538a6104dd16c2b3584d327b116bc8c941a1f7
SamedAli <9093568+SamedAli@users.noreply.github.com>
2025-06-05 09:59:44 +0200

    Restructure folders

---
+++ include/camera.h
@@ -0,0 +1,10 @@
+#pragma once
+
+/*
+Camera is responsible for two important jobs.
+    1. Construct and dispatch rays into the world.
+    2. Use the results of these rays to construct the rendered image.
+*/
+class Camera
+{
+};
